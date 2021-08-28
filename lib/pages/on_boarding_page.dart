9a45fc5030ff39f3570e7672c59f1847994dba9c
Arief Syahroni <riefsyah31@gmail.com>
2021-08-28 15:58:33 +0700

    done modified onboardingPage and add some assets or support

---
+++ lib/pages/on_boarding_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:monstera/theme.dart';
+
+class OnBoardingpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/onboard.png',
+            width: double.infinity,
+            height: 370,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 325),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(42),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: defaultMargin,
+                  right: defaultMargin,
+                  top: 36,
+                  bottom: 56,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Don’t just think\ngreen, live green',
+                      style: headerOneTextStyle.copyWith(
+                        color: darkColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 17,
+                    ),
+                    Text(
+                      'I’m planting a tree to remind me to be open and kindhearted.',
+                      style: bodyRegularTextStyle.copyWith(
+                        color: darkColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 17,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 1.5,
+                          width: 15,
+                          color: darkColor.withOpacity(0.4),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Andrea Koehle Jones',
+                          style: bodyRegularTextStyle.copyWith(
+                            color: darkColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 54,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Get Started',
+                          style: bodyBoldTextStyle.copyWith(
+                            color: darkColor,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: primaryColor,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final double defaultMargin = 24.0;
+
+Color primaryColor = Color(0XFF51B1A6);
+Color secondaryColor = Color(0XFF5E5BD9);
+Color darkColor = Color(0XFF24243F);
+Color accentDarkColor = Color(0XFFE1E5EB);
+Color accentColor = Color(0XFFF5F7FA);
+Color lightColor = Color(0XFFFAFCFE);
+Color whiteColor = Color(0XFFFFFFFF);
+Color errorColor = Color(0XFFED4B68);
+Color successColor = Color(0XFF31D0AA);
+Color warningColor = Color(0XFFFFB802);
+Color textDefaultColor = Color(0XFF5A5A75);
+Color subTitleColor = Color(0XFF5A5A75);
+
+TextStyle headerOneTextStyle = GoogleFonts.workSans(
+  fontSize: 31,
+  fontWeight: FontWeight.w600,
+  // letterSpacing: -2,
+);
+
+TextStyle headerTwoTextStyle = GoogleFonts.workSans(
+  fontSize: 25,
+  fontWeight: FontWeight.w500,
+  // letterSpacing: -2,
+);
+
+TextStyle smallTextStyle = GoogleFonts.workSans(
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle captionTextStyle = GoogleFonts.workSans(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.workSans(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 4,
+);
+
+TextStyle titleRegularTextStyle = GoogleFonts.workSans(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  // letterSpacing: -2,
+);
+
+TextStyle titleBoldTextStyle = GoogleFonts.workSans(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  // letterSpacing: -2,
+);
+
+TextStyle bodyRegularTextStyle = GoogleFonts.workSans(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle bodyBoldTextStyle = GoogleFonts.workSans(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  // letterSpacing: -2,
+);
